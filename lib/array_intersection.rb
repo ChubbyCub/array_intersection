abe8823cc5141154f9455ccccbe22876427f05af
Elise Pham <elise.pham88@gmail.com>
2019-03-29 11:11:38 -0700

    completed array_intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,25 @@
 # Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
+require "set"
+
 def intersection(array1, array2)
-  raise NotImplementedError
+  set = Set.new
+  res = Array.new
+
+  if array1.nil? || array2.nil?
+    return res
+  end
+
+  for i in 0...array1.length
+    set << array1[i]
+  end
+
+  for i in 0...array2.length
+    if set.include?(array2[i])
+      res << array2[i]
+    end
+  end
+
+  return res
 end
